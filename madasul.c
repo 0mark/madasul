1871b684c7dcc7a0cc75078eed86d0db21189d3e
mark <mark@b00f7d2d-31f0-44ff-a789-1704c6165fff>
2011-07-08 22:21:12 +0000

    clean up

--- madasul.c
+++ madasul.c
@@ -15,6 +15,7 @@
 #include <sys/wait.h>
 #include <stdarg.h>
 
+/* macros */
 #define LENGTH(X)          (sizeof X / sizeof X[0])
 
 #define BUF_SIZE           1024
@@ -24,84 +25,102 @@
 #define WRITE              1
 #define ERR                2
 
+
+/* enums */
 enum { MPG, OGG, WAV, FLACC, TypeLast };
-enum { STOP, PAUSE, PLAY, NEXT, PREV, DIE, STATUS, CommandLast };
+enum { STOP, PAUSE, PLAY, NEXT, PREV, DIE, STATUS, RND, LIST, CommandLast };
 enum { INC, DEC, SET };
 
+
+/* structs */
 typedef struct track {
     int type;
     char* path;
 } track;
 
-track** tracks;
+
+/* function declarations */
+// helper
+static void die(const char *errstr, ...);
+static void sock_printf(int cmd_sock, const char *format, ...);
+// init
+static int munchIn();
+static int opensock();
+// worker
+static pid_t play(track* track, int* infp, int* outfp);
+static int get_cmd(int *cmd_sock, char *val);
+// player helper
+static void scout(unsigned int whatdo, unsigned int val);
+// threads
+static void* listener();
+static void* player();
+
+
+/* variables */
+// types
 char* typenames[] = { "mp3", "mp2", "ogg", /*"wav", "wave", "flacc", */};
 int types[]       = {  MPG,   MPG,   OGG,  /* WAV,   WAV,    FLACC, */};
-char* ctrl_cmds[] = { "stop", "pause", "play", "next", "prev", "die", "status", };
+// player
 char* play_cmds[][3]    = {
 	{ "/usr/bin/mpg123", "mpg123", "" },
 	{ "/usr/bin/ogg123", "ogg123", "" },
 };
+// commands
+char* ctrl_cmds[] = { "stop", "pause", "play", "next", "prev", "die", "status", "random", "tracklist"};
 
+track** tracks;
 int ctrl_sock     = -1;
 int cur_track, num_tracks;
-//int stop = 0;
 int running = 0;
 pthread_mutex_t lock;
 pid_t player_pid = -1;
+int rnd = 1;
+static const int debug =0;
 
-pid_t play(track* track, int* infp, int* outfp) {
-    int p_stdin[2], p_stdout[2], type = track->type;
-    pid_t pid;
-	char path[BUF_SIZE+2];
 
-    if(pipe(p_stdin) != 0 || pipe(p_stdout) != 0)
-        return -1;
+/* function definitions */
+void die(const char *errstr, ...) {
+	va_list ap;
 
-    pid = fork();
+	va_start(ap, errstr);
+	vfprintf(stderr, errstr, ap);
+	va_end(ap);
+	exit(1);
+}
 
-    if(pid < 0)
-        return pid;
-    else if (pid == 0) {
-        close(p_stdin[WRITE]);
-        dup2(p_stdin[READ], READ);
-        close(p_stdout[READ]);
-        dup2(p_stdout[WRITE], ERR);
-        snprintf(path, BUF_SIZE+2, "\"%s\"", track->path);
-		execl(play_cmds[type][0], play_cmds[type][1], track->path, play_cmds[type][2], NULL);
-        perror("execl");
-        exit(1);
-    }
+void sock_printf(int cmd_sock, const char *format, ...) {
+	char msg[BUF_SIZE];
+	va_list ap;
 
-    if(infp==NULL)
-        close(p_stdin[WRITE]);
-    else
-        *infp = p_stdin[WRITE];
+	va_start(ap, format);
+	vsnprintf(msg, BUF_SIZE, format, ap);
 
-    if(outfp==NULL)
-        close(p_stdout[READ]);
-    else
-        *outfp = p_stdout[READ];
+	if(debug)
+		printf("to socket: %s\n", msg);
 
-    return pid;
+	send(cmd_sock, msg, strlen(msg), MSG_NOSIGNAL);
+	va_end(ap);
 }
 
 int munchIn() {
     char type[10], buffer[BUF_SIZE];
     int i = 0, j = 0, k, len;
 
-    tracks = calloc(sizeof(track*), TRACKS_BUF_SIZE);
-
-    if(tracks == NULL) {
-        perror("Failed to allocate content");
-        return -1;
-    }
+    if((tracks = calloc(sizeof(track*), TRACKS_BUF_SIZE)) == NULL)
+        die("Ex memoria on Mars Error: failed to allocate some memory\n");
 
     while(!feof(stdin)) {
-        if(fscanf(stdin, "%[^\t]\t%[^\n]\n", type, buffer)!=2)
-	    continue;
+        if(fscanf(stdin, "%[^\t]\t%[^\n]\n", type, buffer)!=2) {
+			printf("Runaway Indian in the prairie of Spain Error: failed to read track line %d\n", i);
+			continue;
+		}
         len = strlen(buffer);
-        tracks[i] = calloc(sizeof(track), len + 1);
-        tracks[i]->path = calloc(sizeof(char), len + 1);
+
+        if((tracks[i] = calloc(sizeof(track), len + 1)) == NULL)
+	        die("Ex memoria on Mars Error: failed to allocate some memory\n");
+        if((tracks[i]->path = calloc(sizeof(char), len + 1)) == NULL)
+	        die("Ex memoria on Mars Error: failed to allocate some memory\n");
+
         for(k=0; k<LENGTH(typenames); k++)
             if(strncmp(type, typenames[k], LENGTH(typenames[k]))==0)
                 tracks[i]->type = types[k];
@@ -110,8 +129,9 @@ int munchIn() {
         j++;
         if(j>=TRACKS_BUF_SIZE) {
             track **tmp = tracks;
-            tracks = calloc(sizeof(track*), i + TRACKS_BUF_SIZE - 1);
-            memcpy(tracks, tmp, i - 1);
+            if((tracks = calloc(sizeof(track*), i + TRACKS_BUF_SIZE - 1)) == NULL)
+		        die("Ex memoria on Mars Error: failed to allocate some memory\n");
+            memcpy(tracks, tmp, (i)*sizeof(track*));
             j = 0;
         }
     }
@@ -129,35 +149,55 @@ int opensock() {
     int len, flags;
     struct sockaddr_un saun;
 
-    if((ctrl_sock = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
-        ctrl_sock = -1;
-        printf("Error opening socket.\n");
-	return(0);
-    }
+    if((ctrl_sock = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
+        die("Bare feet in Nakatomi Tower Error: failed to open socket.\n");
 
 	flags = fcntl(ctrl_sock, F_GETFL, 0);
-	//fcntl(ctrl_sock, F_SETFL, flags | O_NONBLOCK);
-
     saun.sun_family = AF_UNIX;
     strcpy(saun.sun_path, SOCKET_ADDRESS);
-
     len = sizeof(saun.sun_family) + strlen(saun.sun_path);
 
-    if(bind(ctrl_sock, (struct sockaddr *)&saun, len)<0) {
-        printf("Error connecting socket.\n");
-        return(0);
-    }
+    if(bind(ctrl_sock, (struct sockaddr *)&saun, len)<0)
+        die("Bare feet in Nakatomi Tower Error: failed to bind socket.\n");
+
 	listen(ctrl_sock, 5);
     return(1);
 }
 
-void die(const char *errstr, ...) {
-	va_list ap;
+pid_t play(track* track, int* infp, int* outfp) {
+    int p_stdin[2], p_stdout[2], type = track->type;
+    pid_t pid;
+	char path[BUF_SIZE+2];
 
-	va_start(ap, errstr);
-	vfprintf(stderr, errstr, ap);
-	va_end(ap);
-	exit(1);
+    if(pipe(p_stdin) != 0 || pipe(p_stdout) != 0)
+        die("No Pizza in the Sewer Error: failed to open pipe\n");
+
+    pid = fork();
+
+    if(pid < 0)
+        die("There is no Fork in Zion Error: failed to fork\n");
+    else if (pid == 0) { // 
+        close(p_stdin[WRITE]);
+        dup2(p_stdin[READ], READ);
+        close(p_stdout[READ]);
+        dup2(p_stdout[WRITE], ERR);
+        snprintf(path, BUF_SIZE+2, "\"%s\"", track->path);
+		execl(play_cmds[type][0], play_cmds[type][1], track->path, play_cmds[type][2], NULL);
+        // we should not be here, it means the exec did not work...
+		die("Conduction error in Cold Mountain: failed to exec\n");
+    }
+
+    if(infp==NULL)
+        close(p_stdin[WRITE]);
+    else
+        *infp = p_stdin[WRITE];
+
+    if(outfp==NULL)
+        close(p_stdout[READ]);
+    else
+        *outfp = p_stdout[READ];
+
+    return pid;
 }
 
 int get_cmd(int *cmd_sock, char *val) {
@@ -167,9 +207,8 @@ int get_cmd(int *cmd_sock, char *val) {
 	int i, n, len, cmd = -1;
 
 	clilen = sizeof(cli_addr);
-	*cmd_sock = accept(ctrl_sock, (struct sockaddr *)&cli_addr, &clilen);
-	if(cmd_sock < 0)
-		die("cant open listen socket\n");
+	if((*cmd_sock = accept(ctrl_sock, (struct sockaddr *)&cli_addr, &clilen)) < 0)
+		die("Bare feet in Nakatomi Tower Error: failed to open a socket to listen\n");
 	n = read(*cmd_sock, buf, BUF_SIZE);
 	buf[n] = 0;
 	val[0] = 0;
@@ -194,74 +233,32 @@ void scout(unsigned int whatdo, unsigned int val) {
 	pthread_mutex_lock(&lock);
 	switch(whatdo) {
 		case INC:
-			cur_track += val;
+			if(rnd) {
+				cur_track = random(); // valid because % num_tracks later. Haha!
+			} else
+				cur_track += val;
 			break;
 		case DEC:
-			cur_track -= val;
+			if(rnd) {
+				cur_track = random(); // valid because % num_tracks later. Haha!
+			} else
+				cur_track -= val;
 			break;
 		case SET:
 			cur_track = val;
 			break;
 	}
-	if(cur_track<=0) cur_track = num_tracks-1;
-	if(cur_track>=num_tracks) cur_track = 0;
+	if(cur_track<0) cur_track = num_tracks - cur_track;
+	if(cur_track>=num_tracks) cur_track = cur_track % num_tracks;
 	pthread_mutex_unlock(&lock);
-}
-
-void* player() {
-	FILE* fp;
-    int num, infp, w, outfp, cmd, stat;
-    char line[BUF_SIZE];
-
-printf("P a\n");
-	if(player_pid>=0)
-		die("pid is already in use\n");
-printf("P b\n");
-
-	pthread_mutex_lock(&lock);
-printf("P c\n");
-	if((player_pid = play(tracks[cur_track], &infp, &outfp))<=0) {
-printf("P d\n");
-		die("cant exec player\n");
-	}
-printf("P e\n");
-	pthread_mutex_unlock(&lock);
-printf("P f\n");
-
-	w = waitpid(player_pid, &stat, 0);
-printf("P g\n");
-	if(WIFEXITED(stat)!=0) // WEXITSTATUS(stat_val)
-		scout(INC, 1);
-printf("P h\n");
-
-	player_pid = -1;
-printf("P i\n");
-
-	close(infp);
-printf("P j\n");
-	close(outfp);
-printf("P k\n");
-}
-
-void sock_printf(int cmd_sock, const char *format, ...) {
-	char msg[BUF_SIZE];
-	va_list ap;
-
-	va_start(ap, format);
-	vsnprintf(msg, BUF_SIZE, format, ap);
-	//printf("%s\n", msg);
-	send(cmd_sock, msg, strlen(msg), MSG_NOSIGNAL);
-	va_end(ap);
 }
 
 void* listener() {
 	char val[BUF_SIZE];
 	int cmd, cmd_sock, i;
-printf("L a\n");
+
 	while(1) {
-printf("L b\n");
 		if((cmd=get_cmd(&cmd_sock, val))>=0) {
-printf("L c %d\n", cmd);
 			switch(cmd) {
 				case STOP:
 					kill(player_pid, SIGKILL);
@@ -281,7 +278,6 @@ printf("L c %d\n", cmd);
 						sock_printf(cmd_sock, "bad value\n");
 					break;
 				case NEXT:
-				printf("---%s---\n", val);
 					if(strlen(val)) i = atoi(val);
 					else i = 1;
 					if(i>0) {
@@ -309,132 +305,71 @@ printf("L c %d\n", cmd);
 				case STATUS:
 					sock_printf(cmd_sock, "Track (%d): %s\n", cur_track, tracks[cur_track]->path);
 					break;
+				case RND:
+					if(strlen(val)) i = atoi(val);
+					else i = !rnd;
+					if(i>=0 && i<=1) {
+						rnd = i;
+						sock_printf(cmd_sock, "OK\n", cur_track, tracks[cur_track]->path);
+					} else
+						sock_printf(cmd_sock, "bad value\n", cur_track, tracks[cur_track]->path);
+					break;
+				case LIST:
+					for(i=0; i<num_tracks; i++)
+						sock_printf(cmd_sock, "Track (%d): %s\n", i, tracks[i]->path);
+					break;
 				default:
-					sock_printf(cmd_sock, "unknown command");
+					sock_printf(cmd_sock, "unknown command\n");
 					break;
 			}
 		} else {
-printf("L d\n");
-			sock_printf(cmd_sock, "invalid command");
+			sock_printf(cmd_sock, "invalid command\n");
 		}
-printf("L e\n");
 		close(cmd_sock);
-printf("L f\n");
 	}
-printf("L g\n");
+}
+
+void* player() {
+	FILE* fp;
+    int num, infp, w, outfp, cmd, stat;
+    char line[BUF_SIZE];
+
+	if(player_pid>=0)
+		die("Heile Welt at Sehbuehl Error: pid is already in use\n");
+
+	pthread_mutex_lock(&lock);
+	player_pid = play(tracks[cur_track], &infp, &outfp);
+	pthread_mutex_unlock(&lock);
+
+	w = waitpid(player_pid, &stat, 0);
+	if(WIFEXITED(stat)!=0) // WEXITSTATUS(stat_val)
+		scout(INC, 1);
+
+	player_pid = -1;
+
+	close(infp);
+	close(outfp);
 }
 
 int main(/*int argc, char *argv[]*/) {
 	pthread_t p_player, p_listener;
-	//pthread_attr_t attr;
+
+	srandom((unsigned int)time(NULL));
 
 	num_tracks = munchIn();
 	printf("munched %d lines\n", num_tracks);
-	if(!opensock())
-	    die("bare feet in nakatomi tower!\n");
+	opensock();
 	printf("opened socket\n");
 
-printf("M a\n");
 	pthread_mutex_init(&lock, NULL);
-printf("M b\n");
-	//pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-printf("M c\n");
 
 	pthread_create(&p_listener, NULL, listener, NULL);
-printf("M c1\n");
-		//pthread_join(p_listener, NULL);
-printf("M d\n");
 	running = 1;
 	while(running==1) {
-printf("M e\n");
 		pthread_create(&p_player, NULL, player, NULL);
-printf("M f\n");
 		pthread_join(p_player, NULL);
-printf("M g\n");
 		sleep(1);
 	}
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-/*
-		if(cmd==STOP) {
-			printf("*puff*\n");
-			close(ctrl_sock);
-			if(pid>0) {
-				close(infp);
-				close(outfp);
-				kill(pid, SIGKILL);
-				x=wait(&stat);
-			}
-			return 0;
-		} else if(cmd==NEXT) {
-			close(infp);
-			close(outfp);
-			kill(pid, SIGKILL);
-			x=wait(&stat);
-			pid = -1;
-		} else if(cmd==PREV) {
-			tracknum-=2;
-			if(tracknum<0) tracknum = num - 1;
-			close(infp);
-			close(outfp);
-			kill(pid, SIGKILL);
-			x=wait(&stat);
-			pid = -1;
-		}
-*/
-
-
-	//pthread_join (p2, NULL);
-
-    /*FILE* fp;
-    int num, infp, x, outfp, cmd, stat;
-    char line[BUF_SIZE];
-    pid_t pid;
-
-    num = munchIn();
-    printf("munched %d lines\n", num);
-
-	opensock();
-	if((pid=play(tracks[tracknum++], &infp, &outfp)) <= 0) {
-		printf("popen2 failed\n");
-		return 0;
-	}
-
-	fp = fdopen(outfp, "r");
-
-	while(1) {
-		if(pid<1) {
-			//printf("playing (%d):\n", tracknum); printf("%s\n", tracks[tracknum]->path);
-			if((pid = play(tracks[tracknum++], &infp, &outfp))<=0) {
-				printf("something failed");
-				return 1;
-			}
-		}
-
-		x = waitpid(pid, &stat, WNOHANG);
-		//printf("(%d): %d, %d\n", pid, x, WIFEXITED(stat));
-		if(x>0 && WIFEXITED(stat)>0) {
-			scout(INC, 1);
-		} else {
-			get_cmd();
-		}
-
-		if(cur_track!=next_track) {
-			close(infp);
-			close(outfp);
-			pid = -1;
-		}
-
-
-		sleep(1);
-	}*/
