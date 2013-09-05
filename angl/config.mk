d62ae1e6ea1a614dfa4c2ebe565949fb9dfa5684
0mark <mark@unserver.de>
2013-09-05 21:28:35 +0200

    angl

---
+++ angl/config.mk
@@ -0,0 +1,22 @@
+# madasuk version
+VERSION = "666.0.1"
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/man
+
+# dbus/notify adds ~250k mem usage
+#NOTIFY_INCS = `pkg-config --cflags dbus-1`
+#NOTIFY_LIBS = `pkg-config --libs dbus-1`
+#NOTIFY_FLAGS = -DUSE_NOTIFY
+#NOTIFY_CFILES = notify.c
+
+INCS = -I. -I/usr/include
+LIBS = -L/usr/lib -lc -lpthread
+
+CPPFLAGS = -DVERSION=\"${VERSION}\"
+CFLAGS = -g -std=c99 -pedantic -Wall -Os -D_REENTRANT ${INCS} ${CPPFLAGS}
+LDFLAGS = ${LIBS}
+
+# compiler and linker
+CC = cc
